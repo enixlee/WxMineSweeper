6d73dbc5b6fbb49521d0267f9ccbc7123fd2789b
enixlee <405284695@qq.com>
2019-02-22 14:06:36 +0800

    增加语言

---
+++ js/config/StringConfig.js
@@ -0,0 +1,50 @@
+/**
+ * Auth: lijiang
+ * Date: 2019/2/22
+ * Description: StringConfig
+ */
+export default {
+  /**
+   * 按钮文字
+   */
+  stringOk: "确定",
+  stringCancel: "取消",
+  /**
+   * 窗口标题
+   */
+  stringGameName: "扫雷",
+  stringCustomFrameTitle: "自定义雷区",
+  stringLeaderboardFrameTitle: "扫雷英雄榜",
+  /**
+   * 菜单文字
+   */
+  stringMenuGame: "游戏",
+  stringMenuStart: "开局",
+  stringMenuTip: "标记",
+  stringMenuColor: "颜色",
+  stringMenuSound: "声音",
+  stringMenuRecord: "扫雷英雄榜",
+  stringMenuExit: "退出",
+  /**
+   *  自定义窗口文字
+   */
+  stringWidth: "宽度",
+  stringHeight: "高度",
+  stringMineNum: "雷数",
+
+  /**
+   * 默认用户名，
+   */
+  stringDefaultUName: "匿名",
+  stringReSetPoint: "重新计分",
+  stringSecond: "秒",
+  /**
+   * 记录面板文字
+   */
+  stringContent1: "已破{$level}记录。",
+  stringContent2: "请留尊姓大名。",
+  /**
+   * 等级菜单区域文字
+   */
+  stringLevel: ["初级", "中级", "高级", "自定义"]
+}
