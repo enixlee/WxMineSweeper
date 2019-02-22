69a56f354b292c13c37d91c8ab437998e1b51d78
enixlee <405284695@qq.com>
2019-02-22 11:43:32 +0800

    fix

---
+++ js/plugin/storage.js
@@ -0,0 +1,81 @@
+/**
+ * Auth: lijiang
+ * Date: 2019/2/22
+ * Description: storage
+ */
+export default class storage {
+  isEnableEncrypt () {
+    return false;
+  }
+
+  getCrypto () {
+    return {
+      MD5: null,
+      stringifyBase64: null,
+      parseBase64: null
+    };
+  }
+
+  /**
+   * 存储器实例
+   * @returns {*}
+   */
+  getStorageIns () {
+    return wx;
+  }
+
+  /**
+   * 加密key
+   * @param key
+   * @returns {*}
+   */
+  encodeStorageKey (key) {
+    return this.isEnableEncrypt() ? this.getCrypto().MD5(key) : key;
+  }
+
+  /**
+   * 加密存储的value
+   * @param value
+   * @returns {string}
+   */
+  encodeStorageValue (value) {
+    let jsonData = JSON.stringify(value);
+    return this.isEnableEncrypt() ? this.getCrypto().stringifyBase64(jsonData) : jsonData;
+  }
+
+  /**
+   * 解密存储的value
+   * @param storageValue
+   * @returns {*}
+   */
+  decodeStorageValue (storageValue) {
+    return this.isEnableEncrypt() ? JSON.parse(this.getCrypto().parseBase64(storageValue)) : JSON.parse(storageValue);
+  }
+
+  getItem (key, D = null) {
+    try {
+      let value = this.getStorageIns().getStorageSync(this.encodeStorageKey(key));
+      // 不处理boolean值
+      value = this.decodeStorageValue(value);
+      return (value !== null || value !== undefined) ? value : D;
+    } catch (e) {
+      return D;
+    }
+  }
+
+  setItem (key, value) {
+    try {
+      this.getStorageIns().setStorageSync(this.encodeStorageKey(key), this.encodeStorageValue(value));
+    } catch (e) {
+      this.getEngine().warn(`缓存错误`, e);
+    }
+  }
+
+  clearItem (key) {
+    this.getStorageIns().removeStorageSync(this.encodeStorageKey(key));
+  }
+
+  clearAll () {
+    this.getStorageIns().clearStorageSync();
+  }
+};
